53638d86fe498d6e1fd35317907fabb990ce7696
Rawi2115 <abdulhamidhaitham215@gmail.com>
2024-04-05 21:14:11 +0300

    added host page

--- index.jsx
+++ index.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
 import Layout from './components/Layout';
+import Dashboard from "./pages/Host/Dashboard"
+import Reviews from './pages/Host/Reviews';
+import Income from './pages/Host/Income';
+import HostLayout from './pages/Host/HostLayout';
 import "./server"
 
 function App() {
@@ -18,6 +22,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+            <Route path="/host/income" element={<Income />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
